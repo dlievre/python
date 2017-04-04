d91ca2ec64ec4b89e8cf5d70c5371f521639b3db
dlievre <42@photeam.com>
2017-04-04 10:54:58 +0200

    Add files via upload

--- d01/all_in.py
+++ d01/all_in.py
@@ -1,12 +1,10 @@
-# -*- coding: utf8 -*-
-def isstate(param, states, capital_cities):
+
+def isState(param, states, capital_cities):
 	if param in states:
-		print(param, 'is a state')
 		return getCapitalOfState(param, states, capital_cities)
 
 def isCapital(param, states, capital_cities):
 	if param in capital_cities.values():
-		print(param, 'is a capital')
 		return getStateOfCapital(param, states, capital_cities)
 
 def getStateOfCapital(param, states, capital_cities):
@@ -15,12 +13,17 @@ def getStateOfCapital(param, states, capital_cities):
 			if capital_cities[key] == param:
 				for key2 in states:
 					if states[key2] == key:
-						print(key2)
 						return key2
 
 def getCapitalOfState(param, states, capital_cities):
 	return capital_cities[states[param]]
-		
+
+def formatWord(param):
+	resulta = param.rstrip()
+	resultb = resulta.lstrip()
+	resultc = resultb.title()
+	return resultc
+	
 if __name__ == '__main__':
 	import sys
 	if len(sys.argv) == 2:
@@ -37,11 +40,17 @@ if __name__ == '__main__':
 			"CO": "Denver"
 			}
 		newlist = sys.argv[1].split(',')
-		for param in newlist:
-			chkState = isstate(param, states, capital_cities)
+		for paramBrut in newlist:
+			param = formatWord(paramBrut)
+			St = 0
+			Ca = 0
+			chkState = isState(param, states, capital_cities)
 			if chkState:
+				St = 1
 				print (getCapitalOfState(param, states, capital_cities), 'is the capital of', param)
 			chkCapital = isCapital(param, states, capital_cities)
 			if chkCapital:
+				Ca = 1
 				print (param, 'is the capital of', getStateOfCapital(param, states, capital_cities))
-
+			if Ca + St == 0 and len(param) >0:
+				print (paramBrut.rstrip().lstrip(),'is neither a capital city nor a state')	
